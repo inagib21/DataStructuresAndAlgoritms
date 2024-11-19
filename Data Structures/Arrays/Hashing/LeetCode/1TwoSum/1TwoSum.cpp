8a0354f0b3eb0cba72f74895e96a013c18226583
inagib21 <inagib21@gmail.com>
2024-11-18 23:53:52 -0500

    add 1

---
+++ Data Structures/Arrays/Hashing/LeetCode/1TwoSum/1TwoSum.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> lookup; // Stores value-to-index mapping
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i]; // Calculate the complement
+
+            // Check if the complement exists in the map
+            if (lookup.find(complement) != lookup.end()) {
+                // If found, return the indices of the complement and the current number
+                return {lookup[complement], i};
+            }
+
+            // Store the current number and its index in the map
+            lookup[nums[i]] = i;
+        }
+
+        // Return an empty vector if no solution is found (problem guarantees one solution)
+        return {};
+    }
+};
